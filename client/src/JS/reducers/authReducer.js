9a950fa8f2d1fff1b4bb0804588c65202ad24bb5
Anis Melliti <ansmeliti@gmail.com>
2020-04-18 22:13:15 +0100

    implement the register action

--- client/src/JS/reducers/authReducer.js
+++ client/src/JS/reducers/authReducer.js
@@ -1,8 +1,32 @@
-const initialState = {};
+import {
+  REGISTER_USER,
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+} from '../constants/actions-types';
 
-//eslint-disable-next-line
+const initialState = {
+  isLoading: false,
+};
+
 const authReducer = (state = initialState, { type, payload }) => {
   switch (type) {
+    case REGISTER_USER:
+      return {
+        ...state,
+        isLoading: true,
+      };
+    case REGISTER_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        user: payload,
+      };
+    case REGISTER_FAIL:
+      return {
+        ...state,
+        isLoading: false,
+        errors: payload,
+      };
     default:
       return state;
   }
